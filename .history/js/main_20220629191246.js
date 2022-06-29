171ed84729c5b9c7feb0c773f23178f006a8b21d
mosak <mosak0217@gmail.com>
2022-06-29 19:17:36 +0900

    2

---
+++ .history/js/main_20220629191246.js
@@ -0,0 +1,78 @@
+$(function(){
+  $('.hamberger_btn').on('click', function() {
+    if ($('#header').hasClass('open')) {
+      $('#header').removeClass('open');
+    } else {
+      $('#header').addClass('open');
+    }
+  });
+
+  $('#mask').on('click', function() {
+    $('#header').removeClass('open');
+  });
+
+  $('#nav a').on('click', function() {
+    $('#header').removeClass('open');
+  });
+
+  $('a[href^="#"]').click(function(){
+    let href= $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
+
+
+
+
+$('.slick_area').slick({
+  infinite: true,
+  autoplay: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  centerMode: true,
+
+  responsive: [
+
+    {
+      breakpoint: 768, 
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 480, 
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+  
+});
+
+
+/*=================================================
+  スクロール時の画像フェード表示
+  ===================================================*/
+  // スクロール時のイベント
+  $(window).scroll(function() {
+    // fadeinクラスに対して順に処理を行う
+    $('.fadein').each(function() {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // fadeinクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+        $(this).css('opacity','1');
+        $(this).css('transform','translateY(0)');
+      }
+    });
+  });
+
+});
+
